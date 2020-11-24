6d4fab4c56c57198b208168f3b51ffad24651f7a
teemvil <tee.viljanen@gmail.com>
2020-11-24 12:52:17 +0200

    Lisätty node express ja server-tiedosto

---
+++ webprojekti/src/server.js
@@ -0,0 +1,13 @@
+let express = require('express');
+let app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World');
+});
+
+let server = app.listen(8081, function () {
+    let host = server.address().address;
+    let port = server.address().port;
+
+    console.log("Example app listening at http://%s:%s", host, port)
+});
