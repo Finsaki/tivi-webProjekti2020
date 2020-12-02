9f9eb37b23c6087f3a3198adf852a28a244708e3
Roosa Laukkanen <roosa.laukkanen@metropolia.fi>
2020-12-02 11:49:10 +0200

    Lisätty kehotus tsekkaamaan oma varaus

--- webprojekti/src/components/VarausOnnistui.vue
+++ webprojekti/src/components/VarausOnnistui.vue
@@ -2,16 +2,16 @@
     <div>
         <p>Varaus luotu onnistuneesti!</p>
         <p>Varauksesi numero on <b>{{varausnro}}</b></p>
+        <p>Voit tarkastella varauksesi tietoja Oma varaus -sivulta.</p>
     </div>
-
 </template>
 
 <script>
   export default {
     name: 'varaus-onnistui',
     props: {
-      varausnro: null
-    }
+      varausnro: Number,
+    },
   };
 </script>
 
